373c3989d897f4d83ea165cd99bb7240073e0611
simonleary-umass-edu <simonleary@umass.edu>
2023-07-05 17:45:15 +0000

    add python json code

---
+++ make-json/lmod-json2my-json.py
@@ -0,0 +1,85 @@
+#!/usr/bin/env python3
+import os
+import json
+
+# when a hidden module adds a new branch to the hierarchy,
+# Lmod spider does not give the hidden property to the modules in that new branch
+HIDDEN_PARENT_DIRS = [
+    "/modules/spack/legacy-microarch/share/spack/modules/linux-ubuntu20.04-cascadelake",
+    "/modules/spack/legacy-microarch/share/spack/modules/linux-ubuntu20.04-haswell",
+    "/modules/spack/legacy-microarch/share/spack/modules/linux-ubuntu20.04-icelake",
+    "/modules/spack/legacy-microarch/share/spack/modules/linux-ubuntu20.04-skylake_avx512",
+    "/modules/spack/legacy-microarch/share/spack/modules/linux-ubuntu20.04-x86_64",
+    "/modules/spack/legacy-microarch/share/spack/modules/linux-ubuntu20.04-zen",
+    "/modules/spack/legacy-microarch/share/spack/modules/linux-ubuntu20.04-zen2",
+    "/modules/uri_modulefiles/all",
+    "/modules/uri_modulefiles"
+]
+
+def nested_dict_append(_dict, key1, key2, value):
+    if key1 not in _dict.keys():
+        _dict[key1] = {}
+    if key2 not in _dict[key1].keys():
+        _dict[key1][key2] = []
+    _dict[key1][key2].append(value)
+
+# build the dicts
+hidden_modules = {}
+non_hidden_modules = {}
+#for path in sys.argv[1:]:
+for path in ["/home/simonleary_umass_edu/ondemand/dev/modules/make-json/x86_64"]:
+    if not os.path.isfile(path):
+        raise RuntimeError(f"\"{path}\" is not a file!")
+    with open(path, 'r') as json_file:
+        json_data = json.load(json_file)
+    for module_name, module_info in json_data.items():
+        for modulefile, modulefile_info in module_info.items():
+            parent_dir = modulefile_info["mpath"]
+            name = os.path.basename(os.path.dirname(modulefile))
+            version = modulefile_info["Version"]
+            if modulefile_info["hidden"]:
+                nested_dict_append(hidden_modules, parent_dir, name, version)
+            else:
+                nested_dict_append(non_hidden_modules, parent_dir, name, version)
+
+# hide the hidden directories
+for dir in HIDDEN_PARENT_DIRS:
+    hidden_modules[dir] = non_hidden_modules[dir]
+    non_hidden_modules.pop(dir)
+
+# remove any version that is just "latest"
+for _dict in [hidden_modules, non_hidden_modules]:
+    for parent_dir, name2versions in _dict.items():
+        for name, versions in name2versions.items():
+            if "latest" in versions:
+                _dict[parent_dir][name] = [x for x in versions if x != "latest"]
+
+# remove duplicate modules
+for _dict in [hidden_modules, non_hidden_modules]:
+    for parent_dir, name2versions in _dict.items():
+        for name, versions in name2versions.items():
+            if len(versions) > 1:
+                _dict[parent_dir][name] = [*set(versions)]
+
+# convert single-item lists to just single items ["8.0"] -> "8.0"
+for _dict in [hidden_modules, non_hidden_modules]:
+    for parent_dir, name2versions in _dict.items():
+        for name, versions in name2versions.items():
+            if len(versions) == 1:
+                _dict[parent_dir][name] = versions[0]
+
+# put parent directories in order of how many modules they provide
+for _dict in [hidden_modules, non_hidden_modules]:
+    _dict = dict(sorted(_dict.items(), key=lambda item: len(item[1])))
+
+# put modules in alphabetical order
+for _dict in [hidden_modules, non_hidden_modules]:
+    for parent_dir, name2versions in _dict.items():
+        #_dict[parent_dir] = dict(sorted(name2versions.items(), key=lambda item: item[0]))
+        _dict[parent_dir] = dict(sorted(name2versions.items()))
+
+with open("hierarchy.json", 'w') as json_out_file:
+    json.dump(non_hidden_modules, json_out_file)
+
+with open("hidden-hierarchy.json", 'w') as json_out_file:
+    json.dump(hidden_modules, json_out_file)
