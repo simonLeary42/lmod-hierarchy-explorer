900b6b5d3aace2cde98d999b458c7ba8d2037d27
simonleary-umass-edu <simonleary@umass.edu>
2023-07-05 16:53:59 +0000

    snake_case

--- app.js
+++ app.js
@@ -12,34 +12,32 @@ function read_file(path, encoding) {
     return fs.readFileSync(path, encoding, flag = 'r');
 }
 
-const jsonData = read_file(relative_path("public/hierarchy.json"), "utf-8");
-const jsonData_hidden = read_file(relative_path("public/hidden-hierarchy.json"), "utf-8");
+const json_data = read_file(relative_path("public/hierarchy.json"), "utf-8");
+const json_data_hidden = read_file(relative_path("public/hidden-hierarchy.json"), "utf-8");
 const index_body = read_file(relative_path("public/body.ejs"), "utf-8");
 
-var baseUri = process.env.PASSENGER_BASE_URI || '/';
+var base_uri = process.env.PASSENGER_BASE_URI || '/';
 app.set('view engine', 'ejs');
 app.get('*', (req, res) => {
-    if (!req.url.includes(baseUri)) {
+    if (!req.url.includes(base_uri)) {
         err_msg = `\
             invalid request "${req.url}"\n\
-            request should contain "${baseUri}"\
+            request should contain "${base_uri}"\
         `
         res.status(404).send(err_msg);
     }
-    modified_req = req.url.replace(baseUri, '');
+    modified_req = req.url.replace(base_uri, '');
     modified_req = path.join("public", modified_req);
-    // default OOD request
+    // default ood request
     if (modified_req == "public") {
-        rendered_body = ejs.render(index_body,
-            {
-                JSONDATA: JSON.stringify(jsonData),
-                JSONDATA_HIDDEN: JSON.stringify(jsonData_hidden)
+        rendered_body = ejs.render(index_body, {
+                JSONDATA: JSON.stringify(json_data),
+                JSONDATA_HIDDEN: JSON.stringify(json_data_hidden)
             }
         )
-        res.render(relative_path("/public/ood-header"),
-            {
+        res.render(relative_path("/public/ood-header"), {
                 title: "Module Explorer",
-                baseUri: "https://ood.unity.rc.umass.edu/pun/dev/modules4/",
+                base_uri: "https://ood.unity.rc.umass.edu/pun/dev/modules4/",
                 body: rendered_body
             }
         )
@@ -50,9 +48,9 @@ app.get('*', (req, res) => {
         content = read_file(request_path);
         res.send(content);
     } catch {
-        res.status(404).send(`Failed to read file "${request_path}"`);
+        res.status(404).send(`failed to read file "${request_path}"`);
     }
 });
 app.listen(3000, () => {
-    console.log('Server running on port 3000');
+    console.log('server running on port 3000');
 });
