0320b24edd29cbe8e95b8a4fb860f8b53d33084c
simonleary-umass-edu <simonleary@umass.edu>
2023-07-05 16:47:28 +0000

    auto format

--- app.js
+++ app.js
@@ -4,22 +4,22 @@ const path = require('path');
 const fs = require('fs');
 const ejs = require('ejs');
 
-function relative_path(_path){
+function relative_path(_path) {
     return path.join(__dirname, _path);
 }
 
 function read_file(path, encoding) {
-    return fs.readFileSync(path, encoding, flag='r');
+    return fs.readFileSync(path, encoding, flag = 'r');
 }
 
 const jsonData = read_file(relative_path("public/hierarchy.json"), "utf-8");
 const jsonData_hidden = read_file(relative_path("public/hidden-hierarchy.json"), "utf-8");
 const index_body = read_file(relative_path("public/body.ejs"), "utf-8");
 
-var baseUri   = process.env.PASSENGER_BASE_URI || '/';
+var baseUri = process.env.PASSENGER_BASE_URI || '/';
 app.set('view engine', 'ejs');
 app.get('*', (req, res) => {
-    if(!req.url.includes(baseUri)){
+    if (!req.url.includes(baseUri)) {
         err_msg = `\
             invalid request "${req.url}"\n\
             request should contain "${baseUri}"\
@@ -29,7 +29,7 @@ app.get('*', (req, res) => {
     modified_req = req.url.replace(baseUri, '');
     modified_req = path.join("public", modified_req);
     // default OOD request
-    if (modified_req == "public"){
+    if (modified_req == "public") {
         rendered_body = ejs.render(index_body,
             {
                 JSONDATA: JSON.stringify(jsonData),
@@ -46,7 +46,7 @@ app.get('*', (req, res) => {
         return;
     }
     request_path = relative_path(modified_req);
-    try{
+    try {
         content = read_file(request_path);
         res.send(content);
     } catch {
